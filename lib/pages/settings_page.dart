f7f2ce2260ef1b39c7b1eeb4911ac109bd348fc8
jsmile <jsmile@naver.com>
2023-08-22 17:41:30 +0900

    copy basic open_weather_cubit

---
+++ lib/pages/settings_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:open_weather_cubit/cubits/temp_settings/temp_settings_cubit.dart';
+
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Settings'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: ListTile(
+          // leading: const FlutterLogo(),  // leading 은 title 앞에 배치됨.
+          title: const Text('Temperature Unit'),
+          subtitle: const Text('defalut: Celcius'),
+          trailing: Switch(
+            value: context.watch<TempSettingsCubit>().state.tempUnit ==
+                TempUnit.celcius,
+            // parm 을 사용하지 않을 것이므로 _ 로 표시.
+            onChanged: (_) {
+              context.read<TempSettingsCubit>().toggelTempUnit();
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/repositories/weather_repository.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:open_weather_cubit/exceptions/weather_exception.dart';
+import 'package:open_weather_cubit/models/custom_error.dart';
+import 'package:open_weather_cubit/models/weather.dart';
+import 'package:open_weather_cubit/services/weather_api_service.dart';
+
+import '../models/direct_geocoding.dart';
+import '../utils/ansi_color.dart';
+
+class WeatherRepository {
+  final WeatherApiService weatherApiService;
+
+  WeatherRepository({required this.weatherApiService});
+
+  @override
+  String toString() =>
+      'WeatherRepository(weatherApiService: $weatherApiService)';
+
+  Future<Weather> fetchWeather(String city) async {
+    try {
+      final DirectGeocoding directGeocoding =
+          await weatherApiService.getDirectGeocoding(city);
+      // print('### DirectGeocoding: $directGeocoding');
+      debugPrint(info('### DirectGeocoding: $directGeocoding'));
+
+      final Weather tempWeather =
+          await weatherApiService.getWeather(directGeocoding);
+      // print('### tempWeather: $tempWeather');
+      debugPrint(info('### tempWeather: $tempWeather'));
+
+      //getDirectGeocoding() 지역명과 getWeather() 의 지역명이 다를 수 있어서
+      //name 과 country 등을 directGeocoding 의 항목으로 변경함.
+      final Weather weather = tempWeather.copyWith(
+        name: directGeocoding.name,
+        country: directGeocoding.country,
+        state: directGeocoding.state,
+      );
+      // print('### weather: $weather');
+      // debugPrint(success('### weather: $weather'));
+
+      return weather;
+    } on WeatherException catch (e) {
+      throw CustomError(errMag: e.message);
+    } catch (e) {
+      throw CustomError(errMag: e.toString());
+    }
+  }
+}
